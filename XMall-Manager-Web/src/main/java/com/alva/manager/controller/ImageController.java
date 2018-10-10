7f3f4907b34c63656edbb04356ccc0d2e170ff57
穆国超 <muguochao@qijiayoudao.com>
2018-10-10 18:54:56 +0800

    Xmall-Mine manager --> RedisController and ContentService

---
+++ XMall-Manager-Web/src/main/java/com/alva/manager/controller/ImageController.java
@@ -0,0 +1,16 @@
+package com.alva.manager.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+
+/**
+ * <一句话描述>,
+ * <详细介绍>,
+ *
+ * @author 穆国超
+ * @since 设计wiki | 需求wiki
+ */
+@Controller
+@Api(description = "图片上传统一接口")
+public class ImageController {
+}
