961cdf86ababc1157859dd5b471294d97b3b62e8
穆国超 <muguochao@qijiayoudao.com>
2018-10-11 17:55:51 +0800

    Xmall-Mine manager --> ItemCatController

--- XMall-Manager-Web/src/main/java/com/alva/manager/controller/ItemCatController.java
+++ XMall-Manager-Web/src/main/java/com/alva/manager/controller/ItemCatController.java
@@ -1,8 +1,17 @@
 package com.alva.manager.controller;
 
+import com.alva.common.pojo.ZTreeNode;
+import com.alva.manager.service.ItemCatService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <一句话描述>,
@@ -14,4 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Api(description = "商品分类信息")
 public class ItemCatController {
+
+    @Autowired
+    private ItemCatService itemCatService;
+
+    @RequestMapping(value = "/item/cat/list", method = RequestMethod.GET)
+    @ApiOperation(value = "通过父ID获取商品分类列表")
+    public List<ZTreeNode> getItemCatList(@RequestParam(name = "id", defaultValue = "0") int parentId) {
+        List<ZTreeNode> itemCatList = itemCatService.getItemCatList(parentId);
+        return itemCatList;
+    }
 }
