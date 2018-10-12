516291243f27514c60d0ce17626f86ca3de197d9
穆国超 <muguochao@qijiayoudao.com>
2018-10-12 18:42:48 +0800

    Xmall-Mine manager --> ItemServiceImpl

--- XMall-Manager/XMall-Manager-dao/src/main/java/com/alva/manager/mapper/TbItemMapper.java
+++ XMall-Manager/XMall-Manager-dao/src/main/java/com/alva/manager/mapper/TbItemMapper.java
@@ -31,4 +31,12 @@ public interface TbItemMapper {
 
     List<TbItem> selectItemByCondition(@Param("cid") int cid, @Param("search") String search,
                                        @Param("orderCol") String orderCol, @Param("orderDir") String orderDir);
+
+    List<TbItem> selectItemByMultiCondition(@Param("cid") int cid,@Param("search") String search,@Param("minDate") String minDate,
+                                            @Param("maxDate") String maxDate,@Param("orderCol") String orderCol,
+                                            @Param("orderDir") String orderDir);
+
+    List<TbItem> selectItemFront(@Param("cid") Long cid,
+                                 @Param("orderCol") String orderCol,@Param("orderDir") String orderDir,
+                                 @Param("priceGt") int priceGt,@Param("priceLte") int priceLte);
 }
