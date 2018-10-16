373350a0514058e99115f126efa1f16c0fbfbecf
穆国超 <muguochao@qijiayoudao.com>
2018-10-16 18:53:20 +0800

    Xmall-Mine manager --> ExpressController

--- XMall-Manager-Web/src/main/java/com/alva/manager/controller/ExpressController.java
+++ XMall-Manager-Web/src/main/java/com/alva/manager/controller/ExpressController.java
@@ -1,9 +1,16 @@
 package com.alva.manager.controller;
 
+import com.alva.common.pojo.DataTablesResult;
+import com.alva.common.pojo.Result;
+import com.alva.common.utils.ResultUtil;
+import com.alva.manager.pojo.TbExpress;
 import com.alva.manager.service.ExpressService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <一句话描述>,
@@ -18,4 +25,41 @@ public class ExpressController {
 
     @Autowired
     private ExpressService expressService;
+
+    @RequestMapping(value = "/express/list",method = RequestMethod.GET)
+    @ApiOperation(value = "获得所有快递")
+    public DataTablesResult addressList(){
+
+        DataTablesResult result = new DataTablesResult();
+        List<TbExpress> list=expressService.getExpressList();
+        result.setData(list);
+        result.setSuccess(true);
+        return result;
+    }
+
+    @RequestMapping(value = "/express/add",method = RequestMethod.POST)
+    @ApiOperation(value = "添加快递")
+    public Result<Object> addTbExpress(@ModelAttribute TbExpress tbExpress){
+
+        expressService.addExpress(tbExpress);
+        return new ResultUtil<Object>().setData(null);
+    }
+
+    @RequestMapping(value = "/express/update",method = RequestMethod.POST)
+    @ApiOperation(value = "编辑快递")
+    public Result<Object> updateAddress(@ModelAttribute TbExpress tbExpress){
+
+        expressService.updateExpress(tbExpress);
+        return new ResultUtil<Object>().setData(null);
+    }
+
+    @RequestMapping(value = "/express/del/{ids}",method = RequestMethod.DELETE)
+    @ApiOperation(value = "删除快递")
+    public Result<Object> delAddress(@PathVariable int[] ids){
+
+        for(int id:ids){
+            expressService.delExpress(id);
+        }
+        return new ResultUtil<Object>().setData(null);
+    }
 }
